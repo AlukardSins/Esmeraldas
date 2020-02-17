154806ecd82eed8abfee1bac19bd5076de00524e
gilbertrendon <jilberlv@gmail.com>
2020-02-17 01:59:38 -0500

    Avances BD

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,53 @@
 module.exports = {
-  db: 'mongodb://root:r00tUdeA@ds263028.mlab.com:63028/emerald44'
+  //db: 'mongodb://root:r00tUdeA@ds263028.mlab.com:63028/emerald44'
+  db: 'mongodb://jilberlv@gmail.com:steiko480161@ds263028.mlab.com:63028/emerald44'
 }
+
+let express = require('express'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  dataBaseConfig = require('./database/db');
+
+// Connecting mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(dataBaseConfig.db, {
+  useNewUrlParser: true
+}).then(() => {
+    console.log('Database connected sucessfully ')
+  },
+  error => {
+    console.log('Could not connected to database : ' + error)
+  }
+)
+
+// Set up express js port
+const workerRoute = require('../backend/routes/worker.route')
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/angular8-meanstack-angular-material')));
+app.use('/', express.static(path.join(__dirname, 'dist/angular8-meanstack-angular-material')));
+app.use('/api', studentRoute)
+
+// Create port
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// Find 404 and hand over to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
